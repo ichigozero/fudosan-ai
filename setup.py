9aab6b5648ac70c0ed89d3ff00440cefda0c6bee
Gary Sentosa <gary.sentosa@gmail.com>
2021-01-05 12:07:06 +0900

    Add package dependencies to setup.py

--- setup.py
+++ setup.py
@@ -10,9 +10,11 @@ setup(
     zip_safe=False,
     install_requires=[
         'Flask>=1.1.2',
+        'Flask-PyMongo>=2.3.0',
         'numpy>=1.19.1',
         'pandas>=1.0.5',
         'python-dateutil>=2.8.1',
+        'python-dotenv==0.15.0',
         'pymongo>=3.11.2',
         'scikit-learn>=0.23.2',
     ],
